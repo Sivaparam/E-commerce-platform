5c6cb4951831aaa7436b26b4a37288bf5d058654
Sivaranjani Jayaprakash <shiva.ranjani83@gmail.com>
2022-10-15 14:26:18 -0400

    add card popup form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -73,7 +73,6 @@ const Login = (props) => {
                 />
                 <button
                   className="submit-btn"
-                  style={{ cursor: 'pointer' }}
                   type="submit"
                 >
                   Submit
