06ff208ebfc1a7418fabe6975fd8256b1cb0f5dc
Aldo Pelayo <aldoorronaldo@gmail.com>
2022-10-16 14:49:52 -0400

    style changes 10-16

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,9 @@ import Auth from '../utils/auth';
 import { USER_BOARDS } from '../utils/queries';
 import BoardList from '../components/BoardList';
 
+import '../pages/style/style.css';
+
+
 function Home() {
   const { loading, data } = useQuery(USER_BOARDS);
   console.log(data);
@@ -13,8 +16,10 @@ function Home() {
 
   return (
     
-    <main className="flex-row justify-center mb-4">
-      <div className="col-10 col-lg-5">
+    <main 
+    className="flex-row justify-center"
+    >
+      <div >
 
         {Auth.loggedIn() ? (
           <div>
@@ -27,9 +32,28 @@ function Home() {
             )}
           </div>
         ) : (
-          <div>
-            <p> Please login/signup </p>
+          
+        
+        
+          <div class="landingpage">
+          <div class="container">
+              <div class="intro">
+              <h3 class="intro-title">planning made easy</h3>
+     <p>
+         From business, school or personal goals our boards will help. Development awaits,Sign up or login to get started.
+     </p>
+     <p>
+         Login or signup, its free!
+     </p>
+          <div className="landingPage">
+            <div className="landingContent">
+          <img src='https://t3.ftcdn.net/jpg/04/53/15/70/240_F_453157099_WDyakeU2ZoDdLDnfH8gVlhy4zI5fgcwX.jpg' width={300} height={250}></img>
+            </div>
           </div>
+          </div>
+          </div>
+          </div>
+          
         )}
       </div>
     </main>
