e8d8672b0433004038a17d2d7ce44e3c73f00bd0
Sivaranjani Jayaprakash <shiva.ranjani83@gmail.com>
2022-10-18 18:32:09 -0400

    heroku reload() fix

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -95,7 +95,7 @@ function List() {
         } catch (error) {
             console.log(e);
         }
-        window.location.reload(true);
+        window.location.reload(false);
         console.log(`List ${listTitle} created`);
         setListTitle('');
 
